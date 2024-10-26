b02aab834be611e3bb5c2b90070da9f31f194dc1
Steven CHM <stv.chm@gmail.com>
2024-10-27 05:34:46 +1100

    snapshot update

--- Assets/Scripts/InGameController.cs
+++ Assets/Scripts/InGameController.cs
@@ -22,32 +22,25 @@ public class InGameController : MonoBehaviour
         {
             GhostBlueInst = Instantiate(GhostBluePrefab, Vector3.zero, Quaternion.identity, mapTransform);
             GhostBlueInst.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-            var labelCanvas = GhostBlueInst.GetComponentInChildren<Canvas>();
-            labelCanvas.worldCamera = Camera.main;
-            
         }
 
         if (GhostGreenPrefab != null)
         {
             GhostGreenInst = Instantiate(GhostGreenPrefab, mapTransform);
             GhostGreenInst.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-            var labelCanvas = GhostGreenInst.GetComponentInChildren<Canvas>();
-            labelCanvas.worldCamera = Camera.main;
+
         }
 
         if (GhostRedPrefab != null)
         {
             GhostRedInst = Instantiate(GhostRedPrefab, mapTransform);
             GhostRedInst.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-            var labelCanvas = GhostRedInst.GetComponentInChildren<Canvas>();
-            labelCanvas.worldCamera = Camera.main;
+
         }
         if (GhostYellowPrefab != null)
         {
             GhostYellowInst = Instantiate(GhostYellowPrefab, mapTransform);
             GhostYellowInst.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-            var labelCanvas = GhostYellowInst.GetComponentInChildren<Canvas>();
-            labelCanvas.worldCamera = Camera.main;
         }
     }
     
